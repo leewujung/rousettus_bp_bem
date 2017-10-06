93a6cd4cf676b580c2e18a813b0c9b6c39b2eb0b
Wu-Jung Lee <leewujung@gmail.com>
2017-10-05 18:36:41 -0700

    use this to plot beam center for model bp. Note that the set of params (tongue and src locations) are for use with RaColony 20160917 rot-ear model shape, the previous set was for RaColony 20160829 no-ear model shape)

--- fig_model_mf_cntr_srcnum.m
+++ fig_model_mf_cntr_srcnum.m
@@ -1,41 +1,41 @@
 % 2016 08 12  Plot model bp multi-freq contorus
 %             Revised from assemble_pfield_mf_20160803
+% 2017 10 05  Add code to find beam centers and plot them
 
 clear
-usrn = getenv('username');
 % Set up various paths
-if strcmp(usrn,'Wu-Jung')
-    base_path = 'F:\Dropbox\Z_wjlee\projects\rousettus_bp\bp_bem_modeling';
-    addpath('F:\Dropbox\0_CODE\beampattern_other_code');
+if isunix
+    addpath('~/internal_2tb/Dropbox/0_CODE/MATLAB/rbfinterp_v1.2');
+    addpath('~/internal_2tb/Dropbox/0_CODE/MATLAB/saveSameSize');
+    addpath('~/internal_2tb/Dropbox/0_CODE/MATLAB/EllipseDirectFit');
+    addpath('~/internal_2tb/Dropbox/0_CODE/MATLAB/brewermap');
+    save_base_path = '~/internal_2tb/Dropbox/Z_wjlee/projects/rousettus_bp/bp_bem_modeling';
+    model_base_path = '~/internal_2tb/Dropbox/Z_wjlee/projects/rousettus_bp/bp_bem_modeling/';
+else
     addpath('F:\Dropbox\0_CODE\MATLAB\rbfinterp_v1.2');
     addpath('F:\Dropbox\0_CODE\MATLAB\saveSameSize');
     addpath('F:\Dropbox\0_CODE\MATLAB\brewermap');
-else
-    base_path = ['C:\Users\',usrn,'\Dropbox\Z_wjlee\projects\rousettus_bp\bp_bem_modeling'];
-    addpath(['C:\Users\',usrn,'\Dropbox\0_CODE\beampattern_other_code']);
-    addpath(['C:\Users\',usrn,'\Dropbox\0_CODE\MATLAB\rbfinterp_v1.2']);
-    addpath(['C:\Users\',usrn,'\Dropbox\0_CODE\MATLAB\saveSameSize']);
-    addpath(['C:\Users\',usrn,'\Dropbox\0_CODE\MATLAB\brewermap']);
+    save_base_path = 'F:\Dropbox\Z_wjlee\projects\rousettus_bp\bp_bem_modeling';
+    model_base_path = 'F:\Dropbox\Z_wjlee\projects\rousettus_bp\bp_bem_modeling';
 end
 
-
 % Individual pfield path
-indiv_src_path = 'calc_indiv_src_pfield_20160829_Ra-colony-noear-0.5mm';
+% indiv_src_path = 'calc_indiv_src_pfield_20160829_Ra-colony-noear-0.5mm';
 % indiv_src_path = 'calc_indiv_src_pfield_20160829_Ra-colony-0.7mm';
 % indiv_src_path = 'calc_indiv_src_pfield_20160820_bullethead-sc-colony-0.5mm';
 % indiv_src_path = 'calc_indiv_src_pfield_20160817_Ra-colony-0.5mm';
 % indiv_src_path = 'calc_indiv_src_pfield_20160811_Ra224-0.6mm';
 % indiv_src_path = 'calc_indiv_src_pfield_20160812_bullethead-sc-0.5mm';
-% indiv_src_path = 'calc_indiv_src_pfield_20160917_Ra-colony-rotear-0.5mm';
+indiv_src_path = 'calc_indiv_src_pfield_20160917_Ra-colony-rotear-0.5mm';
 ss = strsplit(indiv_src_path,'_');
 model_shape = ss{end};
 bem_calc_date = ss{end-1};
 pfield_size = [73,37];
-lr_opt = 'left';
+lr_opt = 'right';
 
 % Save path
 [~,script_name,~] = fileparts(mfilename('fullpath'));
-save_path = fullfile(base_path,sprintf('%s_%s_%s',script_name,bem_calc_date,model_shape));
+save_path = fullfile(save_base_path,sprintf('%s_%s_%s',script_name,bem_calc_date,model_shape));
 if ~exist(save_path,'dir')
     mkdir(save_path);
 end
@@ -45,10 +45,10 @@ src_incl_str = '3456';
 src_incl = [3,4,5,6];
 % src_incl2 = [1,3,4,6];
 src_loc_path = sprintf('pick_src_loc_%s_%s',bem_calc_date,model_shape);
-% src_loc_file = sprintf('%s_src_loc_newleft34.mat',model_shape);
-src_loc_file = sprintf('%s_src_loc.mat',model_shape);
+src_loc_file = sprintf('%s_src_loc_newleft34.mat',model_shape);
+% src_loc_file = sprintf('%s_src_loc.mat',model_shape);
 % src_loc_file = sprintf('%s_src_loc_many.mat',model_shape);
-SRC = load(fullfile(base_path,src_loc_path,src_loc_file));
+SRC = load(fullfile(model_base_path,src_loc_path,src_loc_file));
 if strcmp(lr_opt,'left')
     nn_all = SRC.idx_left;
 else
@@ -77,8 +77,8 @@ colorset = jet(num_freq);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Tongue clicking location
 % tongue_loc = [0.028,0.000,-0.003];  % Used for bullethead-sc-colony-0.5mm
-% tongue_loc = [0.029,0.000,-0.005];  % Used for Ra-colony-rotear-0.5mm
-tongue_loc = [0.027,0.000,-0.005];  % Used for Ra-colony-noear-0.5mm
+tongue_loc = [0.029,0.000,-0.005];  % Used for Ra-colony-rotear-0.5mm
+% tongue_loc = [0.027,0.000,-0.005];  % Used for Ra-colony-noear-0.5mm
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 save_fname = sprintf('%s_%s_%s_x%05.1f_y%05.1f_z%05.1f_%s_%s_f%skHz',...
@@ -98,8 +98,8 @@ else
     indiv_src_file = sprintf('%s_nn%04d_freq%2.1fkHz.mat',...
         indiv_src_path,nn_all(1),freq_all(1)/1e3);
 end
-load(fullfile(base_path,indiv_src_path,indiv_src_file));  % load in sample bem_results
-load(fullfile(base_path,bem_results.src_path,bem_results.src_param_file));  % load in shape & param
+load(fullfile(model_base_path,indiv_src_path,indiv_src_file));  % load in sample bem_results
+load(fullfile(model_base_path,bem_results.src_path,bem_results.src_param_file));  % load in shape & param
 
 % Sort and select nn_all
 if ~isempty(src_incl)
@@ -153,7 +153,7 @@ for iF=1:num_freq
     k = 2*pi*freq/param.c;
     
     % Assemble total field
-    pfield = nan(size(bem_results.pfield));
+   pfield = nan(size(bem_results.pfield));
     for iN=1:length(nn_all)
         % load individual src pfield
         if rem(freq/1e3,1)==0
@@ -163,7 +163,7 @@ for iF=1:num_freq
             indiv_src_file = sprintf('%s_nn%04d_freq%2.1fkHz.mat',...
                 indiv_src_path,nn_all(iN),freq/1e3);
         end
-        load(fullfile(base_path,indiv_src_path,indiv_src_file));
+        load(fullfile(model_base_path,indiv_src_path,indiv_src_file));
         
         % phase delay
         gap_loc = shape.nodesb(nn_all(iN),1:3);
@@ -202,10 +202,42 @@ for iF=1:num_freq
     c3db_x = smooth(c3db_x,sm_len);
     c3db_y = smooth(c3db_y,sm_len);
     
+    % Find beam center
+    idxnotnan = ~isnan(M.pp_plot);
+    [~,M.vq_norm,M.azq,M.elq] = ...
+        interp_bp(M.az(idxnotnan)/180*pi,M.el(idxnotnan)/180*pi,M.pp_plot(idxnotnan),'rbf');
+    M.azq = M.azq/pi*180;
+    M.elq = M.elq/pi*180;
+
+    % --- max beam energy location
+    xx = M.vq_norm(:);
+    [~,mm_idx] = max(xx);
+    M.max.el = M.elq(mm_idx);
+    M.max.az = M.azq(mm_idx);
+    % --- averaged location of normalized beam energy >-1
+    xx(isnan(xx)) = -inf;
+    [~,sort_idx] = sort(xx,'descend');
+    ii = xx(sort_idx)>-1;
+    M.top.el = mean(M.elq(sort_idx(ii)));
+    M.top.az = mean(M.azq(sort_idx(ii)));
+    % --- fit ellipse
+    % ------- M.el = bem_results.theta
+    % ------- M.az = bem_results.phi
+    [raw,rot_max,rot_elpctr,rot_elpctr_tilt] = ...
+        shift_rotate_bp_composite(M.az,M.el,M.pp,map_proj,0.005);
+    [el_ectr,az_ectr] = minvtran(mstruct,rot_max.E.x0,rot_max.E.y0);  % inverse map projection
+    [M.ectr.el,M.ectr.az] = rotatem(el_ectr,az_ectr,...
+                                      [M.max.el,M.max.az],...
+                                      'inverse','degrees');
+
     % Plot summary figure
     figure(fig_bp)
     plot_model_bp_globe(subplot(3,ceil((num_freq+2)/3),iF),...  % bp globe
         M,cvec,map_proj,az_plot_limit);
+    hold on
+    plotm(M.max.el,-M.max.az,'rx','markersize',8,'linewidth',2);
+    plotm(M.top.el,-M.top.az,'r^','markersize',8,'linewidth',2);
+    plotm(M.ectr.el,-M.ectr.az,'ro','markersize',8,'linewidth',2);
     if rem(freq/1e3,1)==0
         title(sprintf('%02d kHz',freq/1e3));
     else
